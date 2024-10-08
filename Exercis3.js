289a6e2a815a1cacbb63e40d13342d04bd19b488
Maks9m <150911736+Maks9m@users.noreply.github.com>
2024-10-09 01:24:32 +0300

    Add files via upload

---
+++ Exercis3.js
@@ -0,0 +1,8 @@
+const objects = [ false, 23, 'Yes', 3, 4, false, 'Student', 'No', true, 0, true, 'Hi', 5, 5, false]
+const counter = { number: 0, boolean: 0, string: 0}
+
+objects.forEach((a) => {
+    const type = typeof a;
+    ++counter[type];
+});
+console.dir({ counter });
---
+++ Exercise1.js
@@ -0,0 +1,8 @@
+function inc(a)
+{
+    return ++a;
+}
+
+const a = 3;
+const b = inc(a);
+console.dir({a,b});
