bd1c00c432125bedbbb4f2e38be703f1744633bf
Maks9m <maxim.kramarenko8a@gmail.com>
2024-10-10 15:54:33 +0300

    Update Exercise3.js

--- Exercise3.js
+++ Exercise3.js
@@ -1,5 +1,9 @@
 const objects = [ false, 23, 'Yes', 3, 4, false, 'Student', 'No', true, 0, true, 'Hi', 5, 5, false]
-const counter = { number: 0, boolean: 0, string: 0}
+const counter = { 
+    number: 0, 
+    boolean: 0, 
+    string: 0
+}
 
 objects.forEach((a) => {
     const type = typeof a;
