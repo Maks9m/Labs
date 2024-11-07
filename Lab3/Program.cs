10667cfcf2c51b409b6d64989e35dd442a0a4df7
Maks9m <maxim.kramarenko8a@gmail.com>
2024-11-07 02:22:59 +0200

    Ex1

---
+++ Lab3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int Random(int min, int  max)
+{
+    Random random= new Random();
+    return random.Next(min, max);
+}
+Console.WriteLine(Random(1, 10));
+
+int Random1(int max, int min = 0)
+{
+    return Random(min, max);
+}
+Console.WriteLine(Random1(10));
+
+//char characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
