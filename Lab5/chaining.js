b4368b70c76afe1a48830990b02e54f01a3b380f
Maks9m <maxim.kramarenko8a@gmail.com>
2024-12-03 18:36:25 +0200

    nth

--- Lab5/chaining.js
+++ Lab5/chaining.js
@@ -2,4 +2,4 @@ const seq = (f) => (g) => typeof g === "number" ? f(g) : seq(x => f(g(x)));
 
 console.log(seq(x => x * 2)(x => x + 7)(5));
 console.log(seq(x => x + 1)(x => x * 2)(x => x / 3)(x => x - 4)(7));
-console.log(seq(x => x * 10)(x => x - 4)(5));
+console.log(seq(x => x * 10)(x => x - 4)(5));//asdfa
